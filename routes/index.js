66fc0038c17800e8cf522cfab990a61becf61096
Etienne le Roux <efleroux@gmail.com>
2020-04-03 21:17:00 +0200

    Saving

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,52 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Docs2go' });
 });
 
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+/* UPDATE /profile/:user_id */
+router.put('/profile', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot password */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
+});
+
+/* PUT /forgot-password */
+router.put('EDIT /forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+/* GET /reset-password */
+router.get('/reset', (req, res, next) => {
+  res.send('GET /reset');
+});
+
+
 module.exports = router;
 
 // 7 RESTFUL ROUTES
