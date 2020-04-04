81b0cd38871b5d851a7d99da6653a22001aed3bc
Etienne le Roux <efleroux@gmail.com>
2020-04-04 13:24:53 +0200

    Added method to index.js controller to add username email and image to properties od

--- controllers/index.js
+++ controllers/index.js
@@ -4,20 +4,15 @@ const User = require('../models/user');
 
 module.exports = {
 // create a method that we can use with postRegister 
-            postRegister(req, res, next) {
+            async postRegister(req, res, next) {
                 const newUser = new User(
                     {
                         username: req.body.username,
                         email: req.body.email,
                         image: req.body.image
                     });
-                    User.register(newUser, req.body.password, function(err) {
-                        if (err) {
-                            console.log('error while user register!', err);
-                        return next(err);
-                    } 
-                console.log('User registered succesfully!');
-                res.redirect('/');
-            });
-        }
-    }
+                    
+                    await User.register(newUser, req.body.password);
+                    res.redirect('/');
+                    }
+}
